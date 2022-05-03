0152fd12d2da16620a21f970b4125625476e4f23
DiloGritando <cortesruedam@gmail.com>
2022-05-02 21:38:04 -0500

    [NEW] Initial login CSS

--- components/LoginForm/LoginForm.jsx
+++ components/LoginForm/LoginForm.jsx
@@ -1,19 +1,95 @@
-import React from 'react';
+/* eslint-disable @next/next/no-html-link-for-pages */
+import React, { useState } from 'react';
 import styles from "./LoginForm.module.scss";
+import MainButton from "../MainButton/MainButton";
 
 const LoginForm = () => {
+  const [isNew, setIsNew] = useState(true)
   return (
     <div className={styles.LoginForm__container}>
-       <div className={styles.LoginForm__content}>
-        <h3>Register with</h3>
-        <div  className={styles.LoginForm__content__image}>
-          <img
-            src="/assets/71assets/404MissingContent.png"
-            alt="404 in construction"
-          />
-        </div>
-        <h3>Or sign in with your credentials</h3>
-       </div>
+      { isNew === true ?
+        (
+          <div className={styles.LoginForm__content}>
+            <h3>Register with</h3>
+            <div  className={styles.LoginForm__content__image}>
+              <img
+                src="/assets/BrandsLogos/btn_google_signin_light_normal_web.png"
+                alt="Register to the 71Lbs dashboard using your Google credentials"
+              />
+            </div>
+            <h3>Or using your credentials</h3>
+            <form action="">
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder='Name'
+                required
+              />
+              <input
+                type="email"
+                id="mail"
+                name="mail"
+                placeholder='Mail'
+                required
+              />
+              <input
+                type="password"
+                id="psw"
+                name="psw"
+                placeholder="Password"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                required
+              />
+              <input
+                type="checkbox"
+                id="privacy"
+                name="privacy"
+                required
+              />
+              <label htmlFor="privacy">
+                I agree with the{"  "}
+                <a href="/privacy-policy">
+                  Privacy Policy
+                </a>
+              </label>
+            </form>
+            <MainButton
+              buttonText="JOIN"
+              buttonRedirectLink="/"
+            />
+            <p>Already have an account?</p>
+            <div className={styles.LoginForm__content__other}>
+              <p onClick={()=>{setIsNew(false)}}>Login</p>
+              <a href='/'>Forgot password?</a>
+            </div>
+          </div>
+        )
+        :
+        (
+          <div className={styles.LoginForm__content}>
+            <h3>Login with</h3>
+            <div  className={styles.LoginForm__content__image}>
+              <img
+                src="/assets/BrandsLogos/btn_google_signin_light_normal_web.png"
+                alt="Register to the 71Lbs dashboard using your Google credentials"
+              />
+            </div>
+            <h3>Or using your credentials</h3>
+            <MainButton
+              buttonText="JOIN"
+              buttonRedirectLink="/"
+            />
+            <p>New to 71Lbs?</p>
+            <div className={styles.LoginForm__content__other}>
+              <p onClick={()=>{setIsNew(true)}}>Register</p>
+              <a href='/'>Forgot password?</a>
+            </div>
+          </div>
+        )
+      }
+
     </div>
   )
 }
--- components/MainHome/MainHomeMarch2022.js
+++ components/MainHome/MainHomeMarch2022.js
@@ -1,9 +1,11 @@
 import React from "react";
-import ReactPlayer from "react-player";
+/* import ReactPlayer from "react-player"; */
+import dynamic from "next/dynamic";
 import styles from "./MainHomeMarch2022.module.scss";
 
 import LearnMore from "../LearnMoreBtn/LearnMore";
 import MainButton from "../MainButton/MainButton";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 
 const MainHomeMarch2022 = () => {
   return (
@@ -20,15 +22,16 @@ const MainHomeMarch2022 = () => {
         </div>
         <div className={styles.Hero__video}>
           <div className={styles.Hero__video__proper}>
-            {/* <ReactPlayer
+            <ReactPlayer
               className={styles.Hero__video__proper__player}
               width="100%"
               height="100%"
+              min-height="230px"
               url="https://fdbef0a76c08b982db11-0672d8539c129f26551dec2c76292811.ssl.cf5.rackcdn.com/FreeShippingAssessment%20video.mp4"
               controls={true}
               fallback="/assets/71assets/PlaceholderHomeVideo.png"
               light="/assets/71assets/PlaceholderHomeVideo.png"
-            /> */}
+            />
           </div>
           <div className={styles.Hero__video__controller}>
             <button className={styles.Hero__video__controller__btn}>
